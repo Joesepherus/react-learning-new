7681fc6a071e90e10b231e05488c6c4e0397db36
Joseph Maloch <jozefmaloch@gmail.com>
2024-09-24 14:00:41 +0800

    Add Preserved.

---
+++ src/preservingState/NotPreserved/NotPreserved.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import '../styles.css'
+
+export default function NotPreserved() {
+  const [isFancy, setIsFancy] = useState(false);
+  if (isFancy) {
+    return (
+      <div>
+        <Counter isFancy={true} />
+        <label>
+          <input
+            type="checkbox"
+            checked={isFancy}
+            onChange={e => {
+              setIsFancy(e.target.checked)
+            }}
+          />
+          Use fancy styling
+        </label>
+      </div>
+    );
+  }
+  return (
+    // the key makes it so that react has to distinguish between it and the div above in the component tree
+    // also if you do something else other than div, for example span then it will also not preserve the state 
+    // of Counter, cuz the component tree changed in render
+    <div key="123test">
+      <Counter isFancy={false} />
+      <label>
+        <input
+          type="checkbox"
+          checked={isFancy}
+          onChange={e => {
+            setIsFancy(e.target.checked)
+          }}
+        />
+        Use fancy styling
+      </label>
+    </div>
+  );
+}
+
+function Counter({ isFancy }) {
+  const [score, setScore] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  let className = 'counter';
+  if (hover) {
+    className += ' hover';
+  }
+  if (isFancy) {
+    className += ' fancy';
+  }
+
+  return (
+    <div
+      className={className}
+      onPointerEnter={() => setHover(true)}
+      onPointerLeave={() => setHover(false)}
+    >
+      <h1>{score}</h1>
+      <button onClick={() => setScore(score + 1)}>
+        Add one
+      </button>
+    </div>
+  );
+}
